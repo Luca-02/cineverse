34977ecef8ff704492cfbb518aa71d40c50d86fc
Luca Milanesi <milanesiluca2002@gmail.com>
2023-11-15 17:35:26 +0100

    improved auth section architecture

--- app/src/main/java/com/example/cineverse/ViewModel/VerifyEmail/VerifyEmailViewModel.java
+++ app/src/main/java/com/example/cineverse/ViewModel/VerifyEmail/VerifyEmailViewModel.java
@@ -5,21 +5,23 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
-import com.example.cineverse.Repository.Auth.Logged.VerifyEmailRepository;
-import com.example.cineverse.ViewModel.LoggedViewModel;
+import com.example.cineverse.Interface.Logged.IVerifyEmail;
+import com.example.cineverse.Repository.Logged.VerifyEmailRepository;
+import com.example.cineverse.ViewModel.AbstractLoggedViewModel;
 
 /**
- * The VerifyEmailViewModel class extends LoggedViewModel and represents the ViewModel for email
+ * The VerifyEmailViewModel class extends AbstractLoggedViewModel and represents the ViewModel for email
  * verification functionality. It handles user email verification operations, communicates changes
  * in email verification status, email sending status, user logout status, and network errors
  * through LiveData objects. VerifyEmailViewModel integrates with VerifyEmailRepository and
  * triggers the email verification process based on user input.
  */
-public class VerifyEmailViewModel extends LoggedViewModel {
+public class VerifyEmailViewModel extends AbstractLoggedViewModel
+        implements IVerifyEmail {
 
     private final VerifyEmailRepository repository;
-    private final MutableLiveData<Boolean> emailSentLiveData;
-    private final MutableLiveData<Boolean> emailVerifiedLiveData;
+    private MutableLiveData<Boolean> emailSentLiveData;
+    private MutableLiveData<Boolean> emailVerifiedLiveData;
 
     /**
      * Constructs a VerifyEmailViewModel object with the given Application context.
@@ -28,16 +30,23 @@ public class VerifyEmailViewModel extends LoggedViewModel {
      */
     public VerifyEmailViewModel(@NonNull Application application) {
         super(application);
-        this.repository = new VerifyEmailRepository(application);
+        repository = new VerifyEmailRepository(application);
+        super.setLiveData(repository);
         emailSentLiveData = repository.getEmailSentLiveData();
         emailVerifiedLiveData = repository.getEmailVerifiedLiveData();
-        setUserLiveData(repository);
-        setLoggedOutLiveData(repository);
-        setNetworkErrorLiveData(repository);
     }
 
     public MutableLiveData<Boolean> getEmailSentLiveData() {
         return emailSentLiveData;
+    }
+
+    /**
+     * Clears the MutableLiveData instance for email sent status.
+     * This method is typically used when the network error state is consumed, and you don't want
+     * to keep the last state in the LiveData.
+     */
+    public void clearEmailSentLiveData() {
+        emailSentLiveData = new MutableLiveData<>();
     }
 
     public MutableLiveData<Boolean> getEmailVerifiedLiveData() {
@@ -45,8 +54,18 @@ public class VerifyEmailViewModel extends LoggedViewModel {
     }
 
     /**
+     * Clears the MutableLiveData instance for email verification status.
+     * This method is typically used when the network error state is consumed, and you don't want
+     * to keep the last state in the LiveData.
+     */
+    public void clearEmailVerifiedLiveData() {
+        emailVerifiedLiveData = new MutableLiveData<>();
+    }
+
+    /**
      * Initiates the process of sending an email verification to the user's email address.
      */
+    @Override
     public void sendEmailVerification() {
         repository.sendEmailVerification();
     }
@@ -54,6 +73,7 @@ public class VerifyEmailViewModel extends LoggedViewModel {
     /**
      * Initiates the process of reloading the user's data from the server.
      */
+    @Override
     public void reloadUser() {
         repository.reloadUser();
     }
