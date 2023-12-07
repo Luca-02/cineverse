654d5cea4acb088e3f598c036d910be26db84ecd
Luca Milanesi <milanesiluca2002@gmail.com>
2023-12-07 02:11:46 +0100

    fixed PosterRepositoryTest.java

--- app/src/androidTest/java/com/example/cineverse/repository/content/poster/PosterRepositoryTest.java
+++ app/src/androidTest/java/com/example/cineverse/repository/content/poster/PosterRepositoryTest.java
@@ -8,6 +8,10 @@ import android.util.Log;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.example.cineverse.R;
+import com.example.cineverse.data.model.content.Failure;
+import com.example.cineverse.data.model.content.poster.AbstractPosterApiResponse;
+import com.example.cineverse.data.model.content.poster.PosterTv;
+import com.example.cineverse.data.source.content.poster.PosterContentResponseCallback;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,10 +34,20 @@ public class PosterRepositoryTest {
     public void repositoryFetchTest() throws InterruptedException {
         final CountDownLatch latch = new CountDownLatch(1);
 
-        PosterTvRepository repository = new PosterTvRepository(context, response -> {
-            assertNotNull(response);
-            Log.d(TAG, response.toString());
-            latch.countDown();
+        PosterTvRepository repository = new PosterTvRepository(context, new PosterContentResponseCallback<PosterTv>() {
+            @Override
+            public void onResponse(AbstractPosterApiResponse<PosterTv> response) {
+                assertNotNull(response);
+                Log.d(TAG, response.toString());
+                latch.countDown();
+            }
+
+            @Override
+            public void onFailure(Failure failure) {
+                assertNotNull(failure);
+                Log.d(TAG, failure.toString());
+                latch.countDown();
+            }
         });
         repository.fetchPopularTv(context.getString(R.string.language), 1);
         latch.await(5, TimeUnit.SECONDS);
