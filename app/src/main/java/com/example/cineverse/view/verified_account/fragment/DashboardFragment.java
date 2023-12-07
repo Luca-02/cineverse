66e1081dea4ee94ded2d9f88e125ae2edeaff49a
Luca Milanesi <milanesiluca2002@gmail.com>
2023-12-07 16:13:12 +0100

    fixed AppBar account circular logo

--- app/src/main/java/com/example/cineverse/view/verified_account/fragment/DashboardFragment.java
+++ app/src/main/java/com/example/cineverse/view/verified_account/fragment/DashboardFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -14,8 +16,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.example.cineverse.R;
+import com.example.cineverse.data.model.user.User;
+import com.example.cineverse.databinding.CircularLogoLayoutBinding;
 import com.example.cineverse.databinding.FragmentDashboardBinding;
+import com.example.cineverse.view.verified_account.VerifiedAccountActivity;
+import com.example.cineverse.viewmodel.logged.verified_account.VerifiedAccountViewModel;
 
 /**
  * The {@link DashboardFragment} class represents the dashboard of the application.
@@ -23,7 +30,9 @@ import com.example.cineverse.databinding.FragmentDashboardBinding;
  */
 public class DashboardFragment extends Fragment {
 
+    private VerifiedAccountViewModel viewModel;
     private FragmentDashboardBinding binding;
+    private CircularLogoLayoutBinding logoBinding;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -39,7 +48,24 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setActionBar();
         setNavController();
+        setViewModel();
+    }
+
+    /**
+     * Sets up the ActionBar of the activity related to the fragment.
+     */
+    private void setActionBar() {
+        ActionBar actionBar =
+                ((VerifiedAccountActivity) requireActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            LayoutInflater inflater = LayoutInflater.from(requireContext());
+            logoBinding =
+                    CircularLogoLayoutBinding.inflate(inflater);
+            actionBar.setCustomView(logoBinding.getRoot());
+            actionBar.setDisplayShowCustomEnabled(true);
+        }
     }
 
     @Override
@@ -60,6 +86,32 @@ public class DashboardFragment extends Fragment {
         if (navHostFragment != null) {
             NavController navController = navHostFragment.getNavController();
             NavigationUI.setupWithNavController(binding.bottomNav, navController);
+        }
+    }
+
+    /**
+     * Sets up the ViewModel for the fragment.
+     */
+    private void setViewModel() {
+        viewModel = new ViewModelProvider(this).get(VerifiedAccountViewModel.class);
+        viewModel.getUserLiveData().observe(getViewLifecycleOwner(), this::handleUser);
+    }
+
+    /**
+     * Handles the user's authentication status and updates the UI accordingly.
+     *
+     * @param user The current {@link User} user object representing the logged-in user.
+     */
+    private void handleUser(User user) {
+        if (user != null) {
+            String photoUrl = user.getPhotoUrl();
+            if (photoUrl != null) {
+                Glide.with(requireContext())
+                        .load(user.getPhotoUrl())
+                        .into(logoBinding.circularImageView);
+            }
+        } else {
+            viewModel.logOut();
         }
     }
 
