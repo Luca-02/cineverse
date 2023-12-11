d93662f7c08e6e3ce798b3e2f16ef0c6cb6347b5
Ion-Pirnau <i.pirnau@campus.unimib.it>
2023-12-11 14:57:38 +0100

    Set the profile page. Added the Menu on Material ToolBar and Log Out Fuction

--- app/src/main/java/com/example/cineverse/view/verified_account/fragment/account/AccountFragment.java
+++ app/src/main/java/com/example/cineverse/view/verified_account/fragment/account/AccountFragment.java
@@ -1,19 +1,30 @@
 package com.example.cineverse.view.verified_account.fragment.account;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.example.cineverse.R;
 import com.example.cineverse.data.model.user.User;
 import com.example.cineverse.databinding.FragmentAccountBinding;
 import com.example.cineverse.view.verified_account.VerifiedAccountActivity;
+import com.example.cineverse.view.verified_account.fragment.account.utils.AbstractSizeUpdate;
 import com.example.cineverse.viewmodel.logged.verified_account.VerifiedAccountViewModel;
+import com.google.android.material.appbar.AppBarLayout;
 
 /**
  * The {@link AccountFragment} class representing the user account section of the application.
@@ -24,6 +35,15 @@ public class AccountFragment extends Fragment {
     private FragmentAccountBinding binding;
     private VerifiedAccountViewModel viewModel;
 
+    private Toolbar toolbar;
+    private ImageView profile_account_image;
+    private TextView userName;
+    private AppBarLayout appBarLayout;
+    private int initialImageSizePx; // Initial size of profile picture in pixels
+    private int initialTextSizePx; // Initial text size of username in pixels
+    private AbstractSizeUpdate size_updater;
+
+
     public AccountFragment() {
         // Required empty public constructor
     }
@@ -32,20 +52,86 @@ public class AccountFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
+        //binding.toolbar.inflateMenu(R.menu.account_page_section);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setElements(view);
         setViewModel();
+        setActionBar();
+        setAnimation();
         setListeners();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        toolbar.setVisibility(View.VISIBLE);
         binding = null;
+    }
+
+    /*
+    Menu Methods
+     */
+
+    /**
+     * Set Fragment Elements
+     */
+    public void setElements(View view){
+        profile_account_image = view.findViewById(R.id.profile_image);
+        appBarLayout = view.findViewById(R.id.account_appBarLayout);
+        userName = view.findViewById(R.id.userEmail);
+        size_updater = new AbstractSizeUpdate(){};
+    }
+
+    /**
+     * Set my Own Action Bar for the Fragment
+     */
+    private void setActionBar(){
+        toolbar = getActivity().findViewById(R.id.materialToolbar);
+        if (toolbar != null) {
+            toolbar.setVisibility(View.GONE);
+        }
+    }
+
+    public void setAnimation(){
+
+        // Get initial sizes of profileImage and userName from XML
+        initialImageSizePx = profile_account_image.getLayoutParams().width; // Assuming width and height are the same
+        initialTextSizePx = (int) userName.getTextSize(); // Initial text size in pixels
+
+        Log.d("IMAGE", String.valueOf(initialImageSizePx));
+        Log.d("TEXT", String.valueOf(initialTextSizePx));
+        // Add an offset listener to the AppBarLayout
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                // Calculate the percentage of collapse/expansion (-1f to 1f)
+                float collapsePercent = Math.abs(verticalOffset / (float) appBarLayout.getTotalScrollRange());
+
+                // Calculate new sizes based on the collapse percentage
+                int newSize = size_updater.calculateNewSize(collapsePercent, initialImageSizePx, size_updater.dpToPx(getContext(),40)); // Initial and final size in pixels
+                int newTextSize = size_updater.calculateNewTextSize(collapsePercent, initialTextSizePx, size_updater.dpToPx(getContext(),18)); // Initial and final text size in pixels
+
+                // Update the size of profileImage and userName
+                updateSize(newSize, newTextSize);
+            }
+        });
+    }
+
+    /*
+    Update Size of Elments profile image account and Username
+     */
+    private void updateSize(int newSize, int newTextSize) {
+        // Update the size of profileImage and userName
+        profile_account_image.getLayoutParams().width = newSize;
+        profile_account_image.getLayoutParams().height = newSize;
+        profile_account_image.requestLayout();
+
+        userName.setTextSize(TypedValue.COMPLEX_UNIT_PX, newTextSize);
     }
 
     /**
@@ -61,7 +147,17 @@ public class AccountFragment extends Fragment {
      * Sets up click listeners for UI elements in the fragment.
      */
     private void setListeners() {
-        binding.logoutButton.setOnClickListener(view -> viewModel.logOut());
+        binding.toolbar.setOnMenuItemClickListener(menuItem -> {
+            int itemId = menuItem.getItemId();
+            if (itemId == R.id.logoutButton) {
+                // do something for item1
+                viewModel.logOut();
+                return true;
+            } else {
+                // if you do nothing, returning false
+                return false;
+            }
+        });
     }
 
     /**
@@ -71,8 +167,12 @@ public class AccountFragment extends Fragment {
      */
     private void handleUser(User user) {
         if (user != null) {
-            binding.userEmail.setText(String.format("Logged In User: %s", user.getEmail()));
-            binding.logoutButton.setEnabled(true);
+            binding.userEmail.setText(String.format("%s", user.getUsername()));
+            if (user.getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(user.getPhotoUrl())
+                        .into(profile_account_image);
+            }
         } else {
             viewModel.logOut();
         }
---
+++ app/src/main/java/com/example/cineverse/view/verified_account/fragment/account/utils/AbstractSizeUpdate.java
@@ -0,0 +1,22 @@
+package com.example.cineverse.view.verified_account.fragment.account.utils;
+
+import android.content.Context;
+
+public abstract class AbstractSizeUpdate {
+
+    public int calculateNewSize(float collapsePercent, int startSize, int endSize) {
+        // Calculate the size dynamically based on the collapse percentage
+        return (int) (startSize + (endSize - startSize) * collapsePercent);
+    }
+
+    public int calculateNewTextSize(float collapsePercent, int startTextSize, int endTextSize) {
+        // Calculate the text size dynamically based on the collapse percentage
+        return (int) (startTextSize + (endTextSize - startTextSize) * collapsePercent);
+    }
+
+    public int dpToPx(Context context, int dp) {
+        // Convert dp to pixels
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+}
