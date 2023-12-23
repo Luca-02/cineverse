9aed30962dd266d9cb8be766a9080e9898a0d6a5
Ion-Pirnau <i.pirnau@campus.unimib.it>
2023-12-23 16:43:52 +0100

    Branch - Account Settings

--- app/src/main/java/com/example/cineverse/view/verified_account/fragment/account/AccountFragment.java
+++ app/src/main/java/com/example/cineverse/view/verified_account/fragment/account/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.cineverse.view.verified_account.fragment.account;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -18,6 +19,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -31,6 +33,7 @@ import com.example.cineverse.databinding.FragmentAccountBinding;
 import com.example.cineverse.utils.account.JSONParserUtil;
 import com.example.cineverse.utils.account.SizeModifierAccount;
 import com.example.cineverse.utils.account.adapter.RVItem_AccountAdapter;
+import com.example.cineverse.view.settings_account.AccountSettingsActivity;
 import com.example.cineverse.view.verified_account.VerifiedAccountActivity;
 import com.example.cineverse.utils.account.adapter.ScreenSlidePagerAdapter;
 import com.example.cineverse.utils.account.ZoomOutPageTransformer;
@@ -100,7 +103,7 @@ public class AccountFragment extends Fragment {
         setViewModel();
         setViewPager(view);
         setAnimation();
-        setListeners();
+        setToolbarAccountEvent();
     }
 
     @Override
@@ -235,12 +238,16 @@ public class AccountFragment extends Fragment {
     /**
      * Sets up click listeners for UI elements in the fragment.
      */
-    private void setListeners() {
+    private void setToolbarAccountEvent() {
         binding.toolbarAccount.setOnMenuItemClickListener(menuItem -> {
             int itemId = menuItem.getItemId();
             if (itemId == R.id.logoutButton) {
                 // do something for item1
                 viewModel.logOut();
+                return true;
+            } else if (itemId == R.id.settingProfile) {
+                Intent intent = new Intent(getContext(), AccountSettingsActivity.class);
+                startActivity(intent);
                 return true;
             } else {
                 // if you do nothing, returning false
